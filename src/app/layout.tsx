b0723452293e82c5849f0894e989bec570332e2d
sean xiao <zy05530@gmail.com>
2025-02-16 04:02:38 +1100

    test no zoom on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/components/nav-bar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -40,6 +41,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
