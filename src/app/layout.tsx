484126abd7e097c895f8cf807b4229debab85208
sean xiao <zy05530@gmail.com>
2025-02-16 16:45:51 +1100

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import NavBar from "@/components/nav-bar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -52,6 +53,7 @@ export default function RootLayout({
       >
         <NavBar pages={PAGES} />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
