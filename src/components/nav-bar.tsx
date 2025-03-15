d237fc9f148b4218a330cd42faf4280ac2e07c1c
sean xiao <zy05530@gmail.com>
2025-03-15 19:12:46 +1100

    update to tailwind v4

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -54,7 +54,7 @@ export default function NavBar({ pages }: { pages: Page[] }) {
             <NavigationMenuLink asChild key={page.link}>
               <Link
                 href={page.link}
-                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus:bg-zinc-100 focus:text-zinc-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-zinc-100/50 data-[state=open]:bg-zinc-100/50 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus:bg-zinc-800 dark:focus:text-zinc-50 dark:data-[active]:bg-zinc-800/50 dark:data-[state=open]:bg-zinc-800/50"
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-zinc-100 hover:text-zinc-900 focus:bg-zinc-100 focus:text-zinc-900 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 data-active:bg-zinc-100/50 data-[state=open]:bg-zinc-100/50 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus:bg-zinc-800 dark:focus:text-zinc-50 dark:data-active:bg-zinc-800/50 dark:data-[state=open]:bg-zinc-800/50"
                 prefetch={false}
               >
                 {page.title}
